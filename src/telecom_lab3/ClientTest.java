b6a5ecae30f861d382e4746ec5f5a08dad921b22
keertana <keertana0606@gmail.com>
2015-03-09 14:44:08 -0400

    resfsdsfdfs

--- src/telecom_lab3/ClientTest.java
+++ src/telecom_lab3/ClientTest.java
@@ -17,15 +17,7 @@ public class ClientTest extends TestCase {
 	InetAddress address;
 	String ip; 
 	int port = 5001;
-	
-	@Before
-	public void setup() {
-		
-	}
 
-	
-
-	@Test
 	public void testUserExists() throws Exception {
 		try {
 			address = InetAddress.getByName("ecse-489.ece.mcgill.ca");
@@ -35,48 +27,74 @@ public class ClientTest extends TestCase {
 		}
 		Socket socket = new Socket(ip, port);
 		User user = new User("ryan", "singzon", socket);
-		user.createUser("ryan", "singzon");
+		user.createUser();
 		// assuming ryan already exists
 		
-		byte[] readBuf = new byte[1000];
-		int bytesReceived = 0;
-		int bytes = 0;
-
-		
-		try{
-			// Wait for login success message from server
-			DataInputStream input = new DataInputStream(socket.getInputStream());
-
-			// Copy bytes into buffer
-			bytes = input.read(readBuf);
-			
-			// Extract the response from the byte array
-			// Extract the message from the byte array
-			byte[] typeBytes = Arrays.copyOfRange(readBuf, 0, 4);
-			byte[] submessageTypeBytes = Arrays.copyOfRange(readBuf, 4,	8);
-			byte[] sizeBytes = Arrays.copyOfRange(readBuf, 8, 12);
-			
-			int messageType = ByteBuffer.wrap(typeBytes).getInt();
-			int submessageType = ByteBuffer.wrap(submessageTypeBytes).getInt();
-			int dataSize = ByteBuffer.wrap(sizeBytes).getInt();
-			
-			byte[] dataBytes = Arrays.copyOfRange(readBuf, 12, 12 + dataSize);
-			String data = new String(dataBytes);
-			
-			System.out.println("Response: ");
-			System.out.println("Type: "+ messageType);
-			System.out.println("Submessage: "+submessageType);
-			System.out.println("Size: "+dataSize);
-			System.out.println("Data: "+data);
-			Message response = new Message(messageType, submessageType, dataSize, data);
-			assertEquals("user already exists", 1, submessageType);
-		}
-		catch(Exception e){
-			
-		}
-		
-		
+		Message message = user.parseResponse();
+		assertEquals("User exists", 1, message.getSubmessageType());
 		socket.close();
+
+	}
+	
+	public void testUserAlreadyLoggedIn() throws Exception {
+		try {
+			address = InetAddress.getByName("ecse-489.ece.mcgill.ca");
+			ip = address.getHostAddress();
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + e.getMessage());
+		}
+		Socket socket = new Socket(ip, port);
+		
+		// assuming ryan already exists
+		
+		User user= new User("ryan","singzon",socket);
+		user.login();
+		User user1 = new User("keertana", "murali", socket);
+		user.createUser();
+		user.parseResponse();
+		Message message = user.parseResponse();
+		assertEquals("User already logged in", 2, message.getSubmessageType());
+		socket.close();
+	
+
+	}
+	
+	public void testdelete() throws Exception {
+		try {
+			address = InetAddress.getByName("ecse-489.ece.mcgill.ca");
+			ip = address.getHostAddress();
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + e.getMessage());
+		}
+		Socket socket = new Socket(ip, port);
+		
+		// assuming ryan already exists
+		
+		User user= new User("ryan","singzon",socket);
+		user.deleteUser();
+		Message message = user.parseResponse();
+		assertEquals("User not logged in", 1, message.getSubmessageType());
+		socket.close();
+		
+	}
+	
+	public void teststorenotloggedin() throws Exception {
+		try {
+			address = InetAddress.getByName("ecse-489.ece.mcgill.ca");
+			ip = address.getHostAddress();
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + e.getMessage());
+		}
+		Socket socket = new Socket(ip, port);
+		
+		// assuming ryan already exists
+		
+		User user= new User("ryan","singzon",socket);
+		user.createStore();
+		Message message = user.parseResponse();
+		assertEquals("User not logged in", 2, message.getSubmessageType());
+		socket.close();
+		
 
 	}
 }
